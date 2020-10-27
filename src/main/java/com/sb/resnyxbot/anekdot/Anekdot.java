296864d28aecc6dfd2ed28263f2b57981d93cdd4
Sergey Bochkov <bochkovsa@gmail.com>
2020-10-27 13:26:26 +0500

    store last anekdot date

--- src/main/java/com/sb/resnyxbot/anekdot/Anekdot.java
+++ src/main/java/com/sb/resnyxbot/anekdot/Anekdot.java
@@ -2,14 +2,17 @@ package com.sb.resnyxbot.anekdot;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 
 import com.jcabi.xml.XMLDocument;
 import com.sb.resnyxbot.ResnyxService;
+import com.sb.resnyxbot.prop.Prop;
 import com.sb.resnyxbot.prop.PropRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -37,32 +40,37 @@ public final class Anekdot implements ResnyxService {
         );
     }
 
-    @Scheduled(cron = "0 0 9 * * *")
+    @Scheduled(cron = "0 0 6-14 * * *")
     public void send() {
-        propRepo
-                .findById("tg.token")
-                .ifPresentOrElse(
-                        tok -> propRepo
-                                .findById("chat.auto.send")
-                                .ifPresentOrElse(
-                                        prop -> {
-                                            for (String chatId : prop.getValue().split(";")) {
-                                                try {
-                                                    String text = String.format("Анекдот дня:%n%s", daily().getText());
-                                                    new SendMessage(
-                                                            tok.getValue(),
-                                                            Long.valueOf(chatId),
-                                                            text
-                                                    ).execute();
-                                                } catch (IOException ex) {
-                                                    LOG.warn(ex.getMessage(), ex);
-                                                }
-                                            }
-                                        },
-                                        () -> LOG.info("chat.auto.send is empty. exit")
-                                ),
-                        () -> LOG.info("tg.token is empty. exit")
-                );
+        Optional<Prop> tgToken = propRepo.findById("tg.token");
+        Optional<Prop> chatId = propRepo.findById("chat.auto.send");
+        if (tgToken.isEmpty()) {
+            LOG.info("tg.token is empty. exit");
+            return;
+        }
+        if (chatId.isEmpty()) {
+            LOG.info("chat.auto.send is empty. exit");
+            return;
+        }
+        Anek daily = daily();
+        Optional<Prop> lastAnek = propRepo.findById("anekdot.lastDate");
+        if (lastAnek.isPresent() && LocalDateTime.parse(lastAnek.get().getValue()).isAfter(daily.getDate())) {
+            LOG.info("lastDate = {}, daily={}. Exit", lastAnek.get(), daily.getDate());
+            return;
+        }
+        for (String chId : chatId.get().getValue().split(";")) {
+            try {
+                String text = String.format("Анекдот дня:%n%s", daily.getText());
+                new SendMessage(
+                        tgToken.get().getValue(),
+                        Long.valueOf(chId),
+                        text
+                ).execute();
+            } catch (IOException ex) {
+                LOG.warn(ex.getMessage(), ex);
+            }
+        }
+        propRepo.save(new Prop("anekdot.lastDate", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)));
     }
 
     @SneakyThrows
