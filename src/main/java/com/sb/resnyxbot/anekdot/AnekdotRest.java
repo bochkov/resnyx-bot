5b7e457e9798bd311447df899b404906801ea8c2
Sergey Bochkov <bochkovsa@gmail.com>
2021-01-28 12:36:45 +0500

    anekdot by id

--- src/main/java/com/sb/resnyxbot/anekdot/AnekdotRest.java
+++ src/main/java/com/sb/resnyxbot/anekdot/AnekdotRest.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnekdotRest {
 
     @PostMapping("/push")
-    public Serializable rest() {
-        return Anek.random();
+    public Serializable rest(@RequestParam(required = false) Integer id) {
+        return id == null ?
+                Anek.random() :
+                Anek.byId(id);
     }
 }
