7f74c2983a46ec82c390c3d13c46af5b46714c9b
Sergey Bochkov <bochkovsa@gmail.com>
2022-02-14 15:20:23 +0500

    fix travis post request param

--- src/main/java/com/sb/resnyxbot/travis/TravisRest.java
+++ src/main/java/com/sb/resnyxbot/travis/TravisRest.java
@@ -2,8 +2,8 @@ package com.sb.resnyxbot.travis;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +15,8 @@ public final class TravisRest {
 
     private final TravisResnyx travisResnyx;
 
-    @PostMapping
-    public void travisNotif(@RequestBody TravisUpd payload) {
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public void travisNotif(TravisUpd payload) {
         travisResnyx.push(payload.msg());
     }
 
